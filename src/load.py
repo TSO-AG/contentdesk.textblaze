3ff6e63930ca334595451f81de2a6054309f8a62
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-03-22 14:21:37 +0100

    Add load function to create rows in Blaze Table

---
+++ src/load.py
@@ -0,0 +1,35 @@
+import json
+from os import getenv
+from dotenv import find_dotenv, load_dotenv
+import boto3
+import requests
+load_dotenv(find_dotenv())
+
+BLAZE_DATABASE_ID = getenv('BLAZE_DATABASE_ID')
+BLAZE_TOKEN = getenv('BLAZE_TOKEN')
+BLAZE_TABLE_ID = getenv('BLAZE_TABLE_ID')
+
+
+# Create Row in Blaze Table
+def createRow(data):
+  url = f"https://data-api.blaze.today/api/database/rows/table/{BLAZE_TABLE_ID}/?user_field_names=true"
+
+  request = requests.post(
+    url,
+    headers={
+        "Authorization": "Token {BLAZE_TABLE_ID}",
+        "Content-Type": "application/json"
+    },
+    json={
+        "sku": data['identifier'],
+        "name": data['values']['name'][0]['data'],
+        "shortDescription": data['values']['shortDescription'][0]['data'],
+        "description": data['values']['description'][0]['data']
+    }
+  )
+  return request.json()
+
+def load(data):
+    for product in data:
+        response = createRow(product)
+        print(response)
