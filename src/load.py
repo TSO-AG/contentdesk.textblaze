49bfdb6e1c8f11f969adc917ab62f900cd2d76e9
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-03-22 14:44:43 +0100

    Fix missing fields in createRow function

--- src/load.py
+++ src/load.py
@@ -11,22 +11,27 @@ BLAZE_TABLE_ID = getenv('BLAZE_TABLE_ID')
 
 # Create Row in Blaze Table
 def createRow(data):
-  url = f"https://data-api.blaze.today/api/database/rows/table/{BLAZE_TABLE_ID}/?user_field_names=true"
+    url = f"https://data-api.blaze.today/api/database/rows/table/{BLAZE_TABLE_ID}/?user_field_names=true"
 
-  request = requests.post(
-    url,
-    headers={
-        "Authorization": f"Token {BLAZE_TOKEN}",
-        "Content-Type": "application/json"
-    },
-    json={
-        "sku": data['identifier'],
-        "name": data['values']['name'][0]['data'],
-        "disambiguatingDescription": data['values']['disambiguatingDescription'][0]['data'],
-        "description": data['values']['description'][0]['data']
-    }
-  )
-  return request.json()
+    if 'disambiguatingDescription' not in data['values']:
+        data['values']['disambiguatingDescription'] = [{'data': ''}]
+    if 'description' not in data['values']:
+        data['values']['description'] = [{'data': ''}]
+
+    request = requests.post(
+        url,
+        headers={
+            "Authorization": f"Token {BLAZE_TOKEN}",
+            "Content-Type": "application/json"
+        },
+        json={
+            "sku": data['identifier'],
+            "name": data['values']['name'][0]['data'],
+            "disambiguatingDescription": data['values']['disambiguatingDescription'][0]['data'],
+            "description": data['values']['description'][0]['data']
+        }
+    )
+    return request.json()
 
 def load(data):
     for product in data:
